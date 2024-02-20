da266fe9dc4222b9e7fb3a876562acac45946f98
Wave Krubb <dis-students@dexon-technology.com>
2024-02-20 08:19:07 +0700

    master data

--- src/router/index.js
+++ src/router/index.js
@@ -41,6 +41,117 @@ const routes = [
       },
     ]
   },
+  {
+    path: "/md",
+    name: "MasterData",
+    component: () =>
+      import("../views/Applications/MasterData/router-template.vue"),
+    // beforeEnter: (to, from, next) => {
+    //   if (store.state.user.role == 'manager' || store.state.user.role == 'admin') next()
+    //   else next('/')
+    // },
+    children: [
+
+      {
+        path: "applicable-status",
+        alias: "Applicable Status",
+        component: () =>
+          import("../views/Applications/MasterData/ApplicableStatus.vue"),
+      },
+      {
+        path: "bottom-type",
+        alias: "Bottom Type",
+        component: () =>
+          import("../views/Applications/MasterData/BottomType.vue"),
+      },
+      {
+        path: "construction-code",
+        alias: "Construction Code",
+        component: () =>
+          import("../views/Applications/MasterData/ConstructionCode.vue"),
+      },
+      {
+        path: "dexon-department",
+        alias: "Dexon Department",
+        component: () =>
+          import("../views/Applications/MasterData/DexonDepartment.vue"),
+      },
+      {
+        path: "dexon-position",
+        alias: "Dexon Position",
+        component: () =>
+          import("../views/Applications/MasterData/DexonPosition.vue"),
+      },
+      {
+        path: "foundation",
+        alias: "Foundation",
+        component: () =>
+          import("../views/Applications/MasterData/Foundation.vue"),
+      },
+      {
+        path: "inspection-code",
+        alias: "Inspection Code",
+        component: () =>
+          import("../views/Applications/MasterData/InspectionCode.vue"),
+      },
+
+      {
+        path: "insulation",
+        alias: "Insulation",
+        component: () =>
+          import("../views/Applications/MasterData/Insulation.vue"),
+      },
+      {
+        path: "prefix",
+        alias: "Prefix",
+        component: () =>
+          import("../views/Applications/MasterData/Prefix.vue"),
+      },
+      {
+        path: "product",
+        alias: "Product",
+        component: () =>
+          import("../views/Applications/MasterData/Product.vue"),
+      },
+
+      {
+        path: "roof-shade",
+        alias: "Roof Shade",
+        component: () =>
+          import("../views/Applications/MasterData/RoofShade.vue"),
+      },
+      {
+        path: "roof-type",
+        alias: "Roof Type",
+        component: () =>
+          import("../views/Applications/MasterData/RoofType.vue"),
+      },
+      {
+        path: "tank-integrity-status",
+        alias: "Tank Integrity Status",
+        component: () =>
+          import("../views/Applications/MasterData/TankIntegrityStatus.vue"),
+      },
+      {
+        path: "tank-internal-pressure",
+        alias: "TankInternalPressure",
+        component: () =>
+          import("../views/Applications/MasterData/TankInternalPressure.vue"),
+      },
+      {
+        path: "tank-material",
+        alias: "Tank Material",
+        component: () =>
+          import("../views/Applications/MasterData/TankMaterial.vue"),
+      },
+      {
+        path: "tank-status",
+        alias: "Tank Status",
+        component: () =>
+          import("../views/Applications/MasterData/TankStatus.vue"),
+      },
+    ]
+  },
 
   // USER ACCOUNT MANAGEMENT
   {
