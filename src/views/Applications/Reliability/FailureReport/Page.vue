6587f3a36ebd840dd4a391ba9fabd7709f0d28e5
matthewcreamer <157339577+matthewcreamer@users.noreply.github.com>
2024-02-21 15:39:44 +0700

    Added table in FailureReport

--- src/views/Applications/Reliability/FailureReport/Page.vue
+++ src/views/Applications/Reliability/FailureReport/Page.vue
@@ -1,7 +1,65 @@
 <template>
   <div class="page-container">
     <div class="page-section">
-      <h1>Failure Report</h1>
+      <div class="table-wrapper">
+        <DxDataGrid
+          id="data-grid-list"
+          key-expr="id"
+          :data-source="testList"
+          :selection="{ mode: 'single' }"
+          :hover-state-enabled="true"
+          :allow-column-reordering="true"
+          :show-borders="true"
+          :show-row-lines="true"
+          :row-alternation-enabled="false"
+          :word-wrap-enabled="true"
+          :column-auto-width="true"
+        >
+          <DxEditing
+            :allow-updating="false"
+            :allow-deleting="false"
+            :allow-adding="true"
+            :use-icons="true"
+            mode="popup"
+          />
+          <DxFilterRow :visible="true" />
+          <DxHeaderFilter :visible="false" />
+          <DxSelection mode="single" />
+          <DxColumn data-field="id_item" caption="Item" :width="70" alignment="center" />
+          <DxColumn data-field="fr_no" caption="FR No." :width="90" alignment="center" />
+          <DxColumn data-field="tag_no" caption="Tag No." :width="100" alignment="center" />
+          <DxColumn data-field="platform" caption="Platform" :width="90" alignment="center" />
+          <DxColumn data-field="unit" caption="Unit" :width="80" alignment="center" />
+          <DxColumn data-field="equipment_type" caption="Equipment Type" :width="90" alignment="center" />
+          <DxColumn data-field="detail" caption="Detail" :min-width="300" alignment="center" />
+          <DxColumn data-field="disc" caption="Disc." :width="90" alignment="center" />
+          <DxColumn data-field="rca_status" caption="RCA Status" :width="80" alignment="center" />
+          <DxColumn data-field="rca_action_status" caption="RCA Action Status" :width="80" alignment="center" />
+
+          <!-- <DxToolbar>
+            <DxItem location="after" template="addRowTemplate" />
+          </DxToolbar>
+          
+
+          <template #addRowTemplate>
+            <DxAddRowButton icon="plus" text="Add New" />
+          </template> -->
+
+          <!-- Configuration goes here -->
+          <!-- <DxFilterRow :visible="true" /> -->
+          <DxScrolling mode="standard" />
+          <DxSearchPanel :visible="true" />
+          <DxPaging :page-size="10" :page-index="0" />
+          <DxPager
+            :show-page-size-selector="true"
+            :allowed-page-sizes="[10, 20, 30]"
+            :show-navigation-buttons="true"
+            :show-info="false"
+            info-text="Page {0} of {1} ({2} items)"
+          />
+          <DxExport :enabled="false" />
+        </DxDataGrid>
+      </div>
     </div>
   </div>
 </template> 
@@ -19,21 +77,25 @@ import "devextreme/dist/css/dx.light.css";
 import { Workbook } from "exceljs";
 import saveAs from "file-saver";
 import { exportDataGrid } from "devextreme/excel_exporter";
+// import DxAddRowButton from "devextreme-vue/button";
 // import { DxItem } from "devextreme-vue/form";
 import {
-  // DxDataGrid,
-//   DxSearchPanel,
-//   DxPaging,
-//   DxPager,
-//   DxScrolling,
-//   DxColumn,
-//   DxExport,
-//   DxToolbar,
-//   DxEditing,
-//   DxLookup,
-//   DxRequiredRule,
-//   DxFormItem,
-//   DxForm
+  DxDataGrid,
+  DxSearchPanel,
+  DxPaging,
+  DxPager,
+  DxScrolling,
+  DxColumn,
+  DxExport,
+  // DxToolbar,
+  DxHeaderFilter,
+  DxSelection,
+  DxEditing,
+  DxFilterRow,
+  // DxLookup,
+  // DxRequiredRule,
+  // DxFormItem,
+  // DxForm
 } from "devextreme-vue/data-grid";
 
 //Structures
@@ -41,20 +103,24 @@ import {
 export default {
   name: "inspection-record",
   components: {
-    // DxDataGrid,
-    // DxSearchPanel,
-    // DxPaging,
-    // DxPager,
-    // DxScrolling,
-    // DxColumn,
-    // DxExport,
+    DxDataGrid,
+    DxSearchPanel,
+    DxPaging,
+    DxPager,
+    DxScrolling,
+    DxColumn,
+    DxExport,
     // DxToolbar,
+    DxHeaderFilter,
+    DxSelection,
     // DxForm,
     // DxItem,
-    // DxEditing,
+    DxEditing,
+    DxFilterRow,
+    // DxAddRowButton,
     // DxLookup,
-    // DxRequiredRule
-    // DxFormItem,
+    // DxRequiredRule,
+    // DxFormItem
   },
   created() {
     this.$store.commit("UPDATE_CURRENT_PAGENAME", {
@@ -62,11 +128,66 @@ export default {
       subpageInnerName: null,
     });
     if (this.$store.state.status.server == true) {
-      this.FETCH_INSP_RECORD();
+      this.testList = [
+        {
+          id: 1,
+          id_item: 1,
+          fr_no: 'FR-00001',
+          tag_no: 'PCV-03302',
+          platform: 'MDPP',
+          unit: '-',
+          equipment_type: 'Choke Valve',
+          detail: 'Phase 3 Choke valve damage issue (MDF01/05, AMA04/12, MTA10)',
+          disc: 'INST',
+          rca_status: 'Done',
+          rca_action_status: 'Done'
+        },
+        {
+          id: 2,
+          id_item: 2,
+          fr_no: 'FR-00002',
+          tag_no: 'LSB-PP-200',
+          platform: 'MDPP',
+          unit: 'V-0331',
+          equipment_type: 'Instrument and Utility',
+          detail: 'Gas Leak from ¾” drain valve upstream of PCV-03302​',
+          disc: 'MECH',
+          rca_status: 'Done',
+          rca_action_status: 'Done'
+        },
+        {
+          id: 3,
+          id_item: 3,
+          fr_no: 'FR-00003',
+          tag_no: 'UPS-PP-230',
+          platform: 'MDPP',
+          unit: 'Low Voltage',
+          equipment_type: 'Switch Board',
+          detail: 'ESD-2 activated due to lost power supply and TG-2 and TG-3 tripped and lost power supply.​',
+          disc: 'ELEC',
+          rca_status: 'Ongoing',
+          rca_action_status: 'Done'
+        },
+        {
+          id: 4,
+          id_item: 4,
+          fr_no: 'FR-00004',
+          tag_no: 'PCV-03302',
+          platform: 'MDPP',
+          unit: 'BC1',
+          equipment_type: 'UPS',
+          detail: 'Variable Inlet Guide Vane (VIGV) Position Controller Shutdown​',
+          disc: 'INST',
+          rca_status: 'Done',
+          rca_action_status: 'Done'
+        }
+      ];
+      // this.FETCH_INSP_RECORD();
     }
   },
   data() {
     return {
+      testList: null,
       inspRecordList: {},
       campaigeList: {},
       dataGridAttributes: {
@@ -205,9 +326,10 @@ export default {
 .page-container {
   width: 100%;
   height: 100%;
-  margin: 0 !important; 
-  // margin-top: 500px;
-  overflow-y: auto;
+  display: grid;
+  grid-template-rows: 51px calc(100vh - 95px);
+  transition: all 0.3s;
+  overflow-y: hidden;
 }
 #report-sheet {
   max-width: 100%;
@@ -251,6 +373,8 @@ export default {
 
 .page-section {
   padding: 20px;
+  overflow-y: auto;
+  grid-row: span 2;
 }
 
 .page-section:last-child {
