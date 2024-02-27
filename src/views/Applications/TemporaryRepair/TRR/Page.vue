65cd13e5ade1fc40491da8231be1a0ac2ae160d5
matthewcreamer <157339577+matthewcreamer@users.noreply.github.com>
2024-02-27 15:26:34 +0700

    Added page for TTR

--- src/views/Applications/TemporaryRepair/TRR/Page.vue
+++ src/views/Applications/TemporaryRepair/TRR/Page.vue
@@ -1,7 +1,62 @@
 <template>
   <div class="page-container">
     <div class="page-section">
-      <h1>Temporary Repair Record</h1>
+      <div class="table-wrapper">
+        <DxDataGrid
+          id="data-grid-list"
+          key-expr="id"
+          :data-source="testList"
+          :selection="{ mode: 'single' }"
+          :hover-state-enabled="true"
+          :allow-column-reordering="true"
+          :show-borders="true"
+          :show-row-lines="true"
+          :row-alternation-enabled="false"
+          :word-wrap-enabled="true"
+          :column-auto-width="true"
+        >
+          <DxEditing
+            :allow-updating="true"
+            :allow-deleting="true"
+            :allow-adding="true"
+            :use-icons="true"
+            mode="popup"
+          />
+          <DxFilterRow :visible="false" />
+          <DxHeaderFilter :visible="false" />
+          <DxSelection mode="single" />
+          <DxColumn data-field="id_item" caption="Item" :width="70" alignment="center" />
+          <DxColumn data-field="temporary_number" caption="Temporary Number" :width="90" alignment="center" />
+          <DxColumn data-field="platform" caption="Platform" :width="90" alignment="center" />
+          <DxColumn data-field="asset_type" caption="Asset Type" :width="90" alignment="center" />
+          <DxColumn data-field="tag_number" caption="Tag Number" :width="120" alignment="center" />
+          <DxColumn data-field="moc_number" caption="MOC Number" :width="120" alignment="center" />
+          <DxColumn data-field="repair_code" caption="Repair Code" :width="120" alignment="center" />
+          <DxColumn data-field="due_date" caption="Due Date" :width="100" alignment="center" />
+          <DxColumn data-field="repair_status" caption="Repair Status" :width="100" alignment="center" />
+          <DxColumn data-field="severity" caption="Severity" :width="80" alignment="center" />
+          <DxColumn data-field="description" caption="Description" :min-width="100" alignment="center" />
+
+          <DxColumn type="buttons">
+            <DxButton name="edit" hint="Edit" icon="edit" />
+            <DxButton name="delete" hint="Delete" icon="trash" />
+          </DxColumn>
+
+          <!-- Configuration goes here -->
+          <!-- <DxFilterRow :visible="true" /> -->
+          <DxScrolling mode="standard" />
+          <DxSearchPanel :visible="true" />
+          <DxPaging :page-size="10" :page-index="0" />
+          <DxPager
+            :show-page-size-selector="true"
+            :allowed-page-sizes="[10, 20, 30]"
+            :show-navigation-buttons="true"
+            :show-info="false"
+            info-text="Page {0} of {1} ({2} items)"
+          />
+          <DxExport :enabled="false" />
+        </DxDataGrid>
+      </div>
     </div>
   </div>
 </template> 
@@ -19,21 +74,26 @@ import "devextreme/dist/css/dx.light.css";
 import { Workbook } from "exceljs";
 import saveAs from "file-saver";
 import { exportDataGrid } from "devextreme/excel_exporter";
+// import DxAddRowButton from "devextreme-vue/button";
 // import { DxItem } from "devextreme-vue/form";
 import {
-//   DxDataGrid,
-//   DxSearchPanel,
-//   DxPaging,
-//   DxPager,
-//   DxScrolling,
-//   DxColumn,
-//   DxExport,
-//   DxToolbar,
-//   DxEditing,
-//   DxLookup,
-//   DxRequiredRule,
-//   DxFormItem,
-//   DxForm
+  DxDataGrid,
+  DxSearchPanel,
+  DxPaging,
+  DxPager,
+  DxScrolling,
+  DxColumn,
+  DxExport,
+  // DxToolbar,
+  DxHeaderFilter,
+  DxSelection,
+  DxEditing,
+  DxFilterRow,
+  DxButton,
+  // DxLookup,
+  // DxRequiredRule,
+  // DxFormItem,
+  // DxForm
 } from "devextreme-vue/data-grid";
 
 //Structures
@@ -41,20 +101,25 @@ import {
 export default {
   name: "inspection-record",
   components: {
-    // DxDataGrid,
-    // DxSearchPanel,
-    // DxPaging,
-    // DxPager,
-    // DxScrolling,
-    // DxColumn,
-    // DxExport,
+    DxDataGrid,
+    DxSearchPanel,
+    DxPaging,
+    DxPager,
+    DxScrolling,
+    DxColumn,
+    DxExport,
     // DxToolbar,
+    DxHeaderFilter,
+    DxSelection,
     // DxForm,
     // DxItem,
-    // DxEditing,
+    DxEditing,
+    DxFilterRow,
+    DxButton,
+    // DxAddRowButton,
     // DxLookup,
-    // DxRequiredRule
-    // DxFormItem,
+    // DxRequiredRule,
+    // DxFormItem
   },
   created() {
     this.$store.commit("UPDATE_CURRENT_PAGENAME", {
@@ -62,11 +127,70 @@ export default {
       subpageInnerName: null,
     });
     if (this.$store.state.status.server == true) {
-      this.FETCH_INSP_RECORD();
+      this.testList = [
+        {
+          id: 1,
+          id_item: 1,
+          temporary_number: 'TEMP-23010',
+          platform: 'MDPP',
+          asset_type: 'Piping',
+          tag_number: '2-GC-J4N-1002',
+          moc_number: 'MOC OFF_23-024',
+          repair_code: 'Welding Patch',
+          due_date: '01 Feb 2024',
+          repair_status: 'Pending',
+          severity: 'P3',
+          description: '-'
+        },
+        {
+          id: 2,
+          id_item: 2,
+          temporary_number: 'TEMP-23009',
+          platform: 'MDPP',
+          asset_type: 'Flowline',
+          tag_number: 'MDPP-03',
+          moc_number: 'MOC OFF_23-023',
+          repair_code: 'Other',
+          due_date: '01 Jan 2024',
+          repair_status: 'Pending',
+          severity: 'P4',
+          description: '-'
+        },
+        {
+          id: 3,
+          id_item: 3,
+          temporary_number: 'TEMP-23008',
+          platform: 'MDPP',
+          asset_type: 'Pressure Vessel',
+          tag_number: 'V-01111',
+          moc_number: 'MOC OFF_23-018',
+          repair_code: 'Other',
+          due_date: '01 Dec 2023',
+          repair_status: 'Completed',
+          severity: 'P3',
+          description: '-'
+        },
+        {
+          id: 4,
+          id_item: 4,
+          temporary_number: 'TEMP-23007',
+          platform: 'MDPP',
+          asset_type: 'Flare',
+          tag_number: '-',
+          moc_number: 'MOC OFF_23-027',
+          repair_code: 'Other',
+          due_date: '01 Jan 2025',
+          repair_status: 'Pending',
+          severity: 'P3',
+          description: '-'
+        },
+      ];
+      // this.FETCH_INSP_RECORD();
     }
   },
   data() {
     return {
+      testList: null,
       inspRecordList: {},
       campaigeList: {},
       dataGridAttributes: {
@@ -205,7 +329,10 @@ export default {
 .page-container {
   width: 100%;
   height: 100%;
-  overflow-y: auto;
+  display: grid;
+  grid-template-rows: 51px calc(100vh - 95px);
+  transition: all 0.3s;
+  overflow-y: hidden;
 }
 #report-sheet {
   max-width: 100%;
@@ -249,6 +376,8 @@ export default {
 
 .page-section {
   padding: 20px;
+  overflow-y: auto;
+  grid-row: span 2;
 }
 
 .page-section:last-child {
