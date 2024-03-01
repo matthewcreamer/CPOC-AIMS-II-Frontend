e1f1900f3fc901c590375dc284b9df7e34785a6f
matthewcreamer <157339577+matthewcreamer@users.noreply.github.com>
2024-03-01 10:34:33 +0700

    test2

--- src/views/404View.vue
+++ src/views/404View.vue
@@ -7,7 +7,7 @@
 
       <v-ons-toolbar-button id="btn" v-on:click="$router.go(-1)">
         <i class="las la-arrow-left"></i>
-        <span>Back to previous pagessttr</span>
+        <span>Back to previous pagessttrr</span>
       </v-ons-toolbar-button>
     </div>
   </div>
