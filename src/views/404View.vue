440319c94837ff19089459da803680e594934d39
matthewcreamer <157339577+matthewcreamer@users.noreply.github.com>
2024-03-01 11:17:56 +0700

    Update 404View.vue

--- src/views/404View.vue
+++ src/views/404View.vue
@@ -7,7 +7,7 @@
 
       <v-ons-toolbar-button id="btn" v-on:click="$router.go(-1)">
         <i class="las la-arrow-left"></i>
-        <span>Back to previous pagesstsd</span>
+        <span>Back to previous pages</span>
       </v-ons-toolbar-button>
     </div>
   </div>
